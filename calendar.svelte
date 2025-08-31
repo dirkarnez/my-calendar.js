af6457ee4a52d93b5d78e35b1265a6f15520d3bf
Dirk Arnez <smalldirkalex@gmail.com>
2025-08-31 18:57:23 +0800

    - add calendar.svelte

---
+++ calendar.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</svelte:head>
+<script>
+
+</script>
+<div class="container">
+	{#each ((year) => {
+    return Array.from({ length: 12 }, (_, i) => {
+        return Array.from({ 
+            length:  new Date(new Date(year, i + 1, 1).getTime() - 1).getDate()
+        }, (_, j) => j + 1)
+    });
+})(2025) as daysInAMonth, i}
+		<div class="row">
+	    <div class="col" style="border: 1px solid rgba(39, 41, 43, 0.1); margin-bottom: 10px">
+			{new Date(2025, i).toLocaleString('default', { month: 'long' })}<br>
+			{#each daysInAMonth as day, j}
+				 <div class="row">
+	        <div class="col" style="border-top: 1px solid rgba(39, 41, 43, 0.1);">
+						{day}
+	        </div>
+	      </div>
+			{/each}
+		   </div>
+	  </div>
+	{/each}
+</div>
